efb33ea617c060a86d4854d507035fffd9043719
Steve White <stevewhitecodes@gmail.com>
2019-03-12 21:36:39 -0500

    Game Complete - Final Push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,157 +1,187 @@
 
-
-
-
-
-
-var questionOne = $('#clyde').attr(true);
-var questionTwo = $('#1982').attr(true);
-var questionThree = $('#computer-space').attr(true);
-var questionFour = $('#bowser-koopa').attr(true);
-var questionFive = $('#tetris').attr(true);
-var questionSix = $('#peach').attr(true);
-var questionSeven = $('#mortal-kombat').attr(true);
-var questionEight = $('#clyde').attr(true);
-var questionNine = $('#clyde').attr(true);
-var questionTen = $('#clyde').attr(true);
+// Variables
 var correctAnswers = 0;
 var wrongAnswers = 0;
+var delayTimer;
+var secondsRemaining = 90;
 
 
 
 
 
-    console.log("Would You LIke To Play A Game!");
+console.log("Would You LIke To Play A Game!");
     
 $(document).ready(function () {
 
+    // Begin the Game - Modal Display
+    $("#begin-button").on("click", function(){
+        $("#start-game").css("display", "none");
+        $(".ghost").prop("checked", false);
+        correctAnswers = 0;
+        wrongAnswers = 0;
+        secondsRemaining = 90;
+        delayTimer = setInterval(function(){
+            secondsRemaining--;
+            $("#timer").text("Time Remaining: " + secondsRemaining + " Seconds");
+            console.log(secondsRemaining);
+            if(secondsRemaining === 0){
+            $("#end-game").css("display", "block");
+            $("#winCaption").text("You got " + correctAnswers + " answers correct out of 10!");
+            $("#loseCaption").text("You got " + wrongAnswers + " answers incorrect out of 10!");
+            submitButton();
+            clearInterval(delayTimer);
+            }
+        }, 1000);
+        
+    });
 
-        $('#submit-button').on('click', function () {
-            var value = $("[name=ghost]:checked").val();
-            if(value === 'sue'){
-                correctAnswers++;
-                console.log('Correct');
-            } else {
-                wrongAnswers++;
-                console.log('Incorrect');
-            }            
-        })
+
+    // Answer Checking
+    // Question One
+    $('#submit-button').on('click', function () {
+        var value = $("[name=ghost]:checked").val();
+        if(value === 'sue'){
+            correctAnswers++;
+            console.log('Correct');
+        } else {
+            wrongAnswers++;
+            console.log('Incorrect');
+        }            
+    });
     
 
+    // Question Two
+    $('#submit-button').on('click', function () {
+        var value = $("[name=year]:checked").val();
+        if(value === '1982'){
+            correctAnswers++;
+            console.log('Correct');
+        } else {
+            wrongAnswers++;
+            console.log('Incorrect');
+        }   
+    });
+
+
+    // Question Three
+    $('#submit-button').on('click', function () {
+        var value = $("[name=arcade-game]:checked").val();
+        if(value === 'computer-space'){
+            correctAnswers++;
+            console.log('Correct');
+        } else {
+            wrongAnswers++;
+            console.log('Incorrect');
+        }   
+    });
+
+    // Question Four
+    $('#submit-button').on('click', function () {
+        var value = $("[name=character]:checked").val();
+        if(value === 'bowser-koopa'){
+            correctAnswers++;
+            console.log('Correct');
+        } else {
+            wrongAnswers++;
+            console.log('Incorrect');
+        }   
+    });
+
+    // Question Five
+    $('#submit-button').on('click', function () {
+        var value = $("[name=best-selling]:checked").val();
+        if(value === 'tetris'){
+            correctAnswers++;
+            console.log('Correct');
+        } else {
+            wrongAnswers++;
+            console.log('Incorrect');
+        }   
+    });
+
+    // Question Six
+    $('#submit-button').on('click', function () {
+        var value = $("[name=princess]:checked").val();
+        if(value === 'peach'){
+            correctAnswers++;
+            console.log('Correct');
+        } else {
+            wrongAnswers++;
+            console.log('Incorrect');
+        }   
+    });
+
+    // Question Seven
+    $('#submit-button').on('click', function () {
+        var value = $("[name=mature-rating]:checked").val();
+        if(value === 'mortal-kombat'){
+            correctAnswers++;
+            console.log('Correct');
+        } else {
+            wrongAnswers++;
+            console.log('Incorrect');
+        }   
+    });
+
+    // Question Eight
+    $('#submit-button').on('click', function () {
+        var value = $("[name=game-system]:checked").val();
+        if(value === 'sega-dreamcast'){
+            correctAnswers++;
+            console.log('Correct');
+        } else {
+            wrongAnswers++;
+            console.log('Incorrect');
+        }   
+    });
+
+    // Question Nine
+    $('#submit-button').on('click', function () {
+        var value = $("[name=average-age]:checked").val();
+        if(value === '35'){
+            correctAnswers++;
+            console.log('Correct');
+        } else {
+            wrongAnswers++;
+            console.log('Incorrect');
+        }   
+    });
+
+    // Question Ten
+    $('#submit-button').on('click', function () {
+        var value = $("[name=scoring]:checked").val();
+        if(value === 'sonic-hedgehog'){
+            correctAnswers++;
+            console.log('Correct');
+        } else {
+            wrongAnswers++;
+            console.log('Incorrect');
+        }   
+    });
+
+    // End Game - Display Score - Display Modal
+    $("#submit-button").on("click", function(){
+        $("#end-game").css("display", "block");
+        $("#loseCaption").text("You got " + wrongAnswers + " answers incorrect out of 10!");
+        $("#winCaption").text("You got " + correctAnswers + " answers correct out of 10!");
+        clearInterval(delayTimer);
+    });
     
-        $('#submit-button').on('click', function () {
-            var value = $("[name=year]:checked").val();
-            if(value === '1982'){
-                correctAnswers++;
-                console.log('Correct');
-            } else {
-                wrongAnswers++;
-                console.log('Incorrect');
-            }   
-        })
-   
+    // Clear Checked Answers - Reset Score - Close Modal
+    $(".close").on("click", function(){
+        $("#end-game").css("display", "none");
+        $(".ghost").prop("checked", false);
+        correctAnswers = 0;
+        wrongAnswers = 0;
+        $("#start-game").css("display", "block");
+    });
 
-   
-        $('#submit-button').on('click', function () {
-            var value = $("[name=arcade-game]:checked").val();
-            if(value === 'computer-space'){
-                correctAnswers++;
-                console.log('Correct');
-            } else {
-                wrongAnswers++;
-                console.log('Incorrect');
-            }   
-        })
-
-        $('#submit-button').on('click', function () {
-            var value = $("[name=character]:checked").val();
-            if(value === 'bowser-koopa'){
-                correctAnswers++;
-                console.log('Correct');
-            } else {
-                wrongAnswers++;
-                console.log('Incorrect');
-            }   
-        })
-
-        $('#submit-button').on('click', function () {
-            var value = $("[name=best-selling]:checked").val();
-            if(value === 'tetris'){
-                correctAnswers++;
-                console.log('Correct');
-            } else {
-                wrongAnswers++;
-                console.log('Incorrect');
-            }   
-        })
-
-        $('#submit-button').on('click', function () {
-            var value = $("[name=princess]:checked").val();
-            if(value === 'peach'){
-                correctAnswers++;
-                console.log('Correct');
-            } else {
-                wrongAnswers++;
-                console.log('Incorrect');
-            }   
-        })
-
-        $('#submit-button').on('click', function () {
-            var value = $("[name=mature-rating]:checked").val();
-            if(value === 'mortal-kombat'){
-                correctAnswers++;
-                console.log('Correct');
-            } else {
-                wrongAnswers++;
-                console.log('Incorrect');
-            }   
-        })
-
-        $('#submit-button').on('click', function () {
-            var value = $("[name=game-system]:checked").val();
-            if(value === 'sega-dreamcast'){
-                correctAnswers++;
-                console.log('Correct');
-            } else {
-                wrongAnswers++;
-                console.log('Incorrect');
-            }   
-        })
-
-        $('#submit-button').on('click', function () {
-            var value = $("[name=average-age]:checked").val();
-            if(value === '35'){
-                correctAnswers++;
-                console.log('Correct');
-            } else {
-                wrongAnswers++;
-                console.log('Incorrect');
-            }   
-        })
-
-        $('#submit-button').on('click', function () {
-            var value = $("[name=scoring]:checked").val();
-            if(value === 'sonic-hedgehog'){
-                correctAnswers++;
-                console.log('Correct');
-            } else {
-                wrongAnswers++;
-                console.log('Incorrect');
-            }   
-        })
-
-        $('#submit-button').on('click', function () {
-            alert("You got " + correctAnswers + " answers correct!");
-            alert("You got " + wrongAnswers + " answers incorrect!");
-            
-        })
-
-        
-
-        
-
-        
-
+    // End Game Modal
+    // var submitButton = function(){
+    //     $("#end-game").css("display", "block");
+    //     $("#loseCaption").text("You got " + wrongAnswers + " answers incorrect out of 10!");
+    //     $("#winCaption").text("You got " + correctAnswers + " answers correct out of 10!");
+    // };
                 
 });
 
